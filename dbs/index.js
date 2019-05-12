07d788e1aff807f2b1b51896fe744232d4fd8153
NguyenDat251 <nguyenquocdat2511998@gmail.com>
2019-05-12 13:55:40 +0700

    add

--- dbs/index.js
+++ dbs/index.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-const PROD_URI = "mongodb://localhost:27017/WebDB"
+const PROD_URI = "mongodb+srv://WebDB:<password>@cluster0-jslyd.mongodb.net/test?retryWrites=true"
 // const MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>"
 
 var dbs = {production: {}};
--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,32 @@ router.get('/login', function(req, res, next) {
 });
 
 router.get('/productInfo', function(req, res, next) {
-  res.render('San_pham', { title: 'Sản phẩm' });
+  const data = {
+    name: 'iPhone XS Max 64 GB',
+    brand: 'Apple',
+    price: '28,790,000',
+    color: ['Bạc', 'Vàng', 'Xám'],
+    shortInfo: ['Hệ điều hành: iOS 12', 'RAM: 4 GB', 'ROM: 64 GB', 'Chip xử lý: A12 Bionic 64-bit 7nm'],
+    info: {
+      screen: '6.5 inches',
+      ram: '4 GB',
+      rom: '64 GB',
+      frontCamera: '7 MP, f / 2.2, 32mm',
+      backCamera: '12 MP',
+      os: 'iOS 12',
+      sim: '1',
+      pin: '3174 mAh'
+    }
+  };
+
+  // var Object={
+  //   title: 'Sản phẩm',
+  //       data: 'haha'
+  // }
+
+  res.render('San_pham', {title: 'Sản phẩm', data});
 });
+
 
 router.get('/bucket', function(req, res, next) {
   res.render('Gio_hang', { title: 'Giỏ hàng' });
